79474d2fbf66997414257f160749142c87bdaa06
chenchangqing <chenchangqing198@126.com>
2015-08-12 23:36:28 +0800

    简单使用CALayer的隐式动画实现进度条

--- example02/example02/ViewController.swift
+++ example02/example02/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var view01 : UIView!
+    var sublayer  : CALayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 初始化view01
+        view01                  = UIView(frame: CGRectMake(16, 36, CGRectGetWidth(view.bounds) - 32, 3))
+        view01.backgroundColor  = UIColor.redColor()
+        
+        // 初始化layer
+        sublayer = CALayer()
+        sublayer.backgroundColor   = UIColor.purpleColor().CGColor
+        sublayer.frame             = CGRectMake(0, 0, 0, 3)
+        
+        // add view01
+        view.addSubview(view01)
+        
+        // add layer
+        view01.layer.addSublayer(sublayer)
+        
+        // 延迟3秒执行隐式动画
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(NSEC_PER_SEC * 3)), dispatch_get_main_queue(), { () -> Void in
+            
+            self.sublayer.frame = CGRectMake(0, 0, 100, 3)
+            self.sublayer.backgroundColor = UIColor.purpleColor().CGColor
+        })
     }
 
     override func didReceiveMemoryWarning() {
