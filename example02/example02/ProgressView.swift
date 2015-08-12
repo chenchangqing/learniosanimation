a45420882401aabe9c5cdc83472d8719902066a5
chenchangqing <chenchangqing198@126.com>
2015-08-13 00:33:45 +0800

    使用CALayer自定义进度条空间完成

---
+++ example02/example02/ProgressView.swift
@@ -0,0 +1,70 @@
+//
+//  ProgressView.swift
+//  example02
+//
+//  Created by green on 15/8/12.
+//  Copyright (c) 2015年 green. All rights reserved.
+//
+
+import UIKit
+
+class ProgressView: UIView {
+    
+    // 进度条
+    private var progressLayer   : CALayer!
+    
+    // 进度条颜色
+    var progressColor           = UIColor.greenColor().CGColor
+    {
+        willSet {
+            progressLayer.backgroundColor = newValue
+        }
+    }
+    
+    // 进度
+    var progressValue           : Float = 0
+    {
+        willSet {
+            
+            if newValue <= 0 {
+                
+                progressLayer.frame = CGRectMake(0, 0, 0, CGRectGetHeight(self.bounds))
+            } else if newValue <= 1 {
+                
+                progressLayer.frame = CGRectMake(0, 0, CGRectGetWidth(self.bounds) * CGFloat(newValue), CGRectGetHeight(self.bounds))
+            } else {
+                
+                progressLayer.frame = CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds))
+            }
+        }
+    }
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+        
+        setup()
+    }
+    
+    /**
+     * 设置layer
+     */
+    private func setup() {
+        
+        self.layer.borderWidth      = 1
+        
+        if progressLayer == nil {
+            
+            progressLayer                   = CALayer()
+            progressLayer.frame             = CGRectMake(0, 0, 50, self.frame.size.height)
+            progressLayer.backgroundColor   = progressColor
+            
+            self.layer.addSublayer(progressLayer)
+        }
+    }
+}
--- example02/example02/ViewController.swift
+++ example02/example02/ViewController.swift
@@ -10,26 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var view01 : UIView!
-    var sublayer  : CALayer!
+    private var view01          : UIView!
+    private var sublayer        : CALayer!
+    private var progressView    : ProgressView!
+    private var timer           : NSTimer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+        
+        progressView01()
+    }
+    
+    /**
+     * 初始化
+     */
+    private func setup() {
+        
         // 初始化view01
         view01                  = UIView(frame: CGRectMake(16, 36, CGRectGetWidth(view.bounds) - 32, 3))
         view01.backgroundColor  = UIColor.redColor()
-        
-        // 初始化layer
-        sublayer = CALayer()
-        sublayer.backgroundColor   = UIColor.purpleColor().CGColor
-        sublayer.frame             = CGRectMake(0, 0, 0, 3)
-        
-        // add view01
         view.addSubview(view01)
         
-        // add layer
+        // 初始化layer
+        sublayer                   = CALayer()
+        sublayer.backgroundColor   = UIColor.purpleColor().CGColor
+        sublayer.frame             = CGRectMake(0, 0, 0, 3)
         view01.layer.addSublayer(sublayer)
+        
+        // 初始化timer
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("progressView02"), userInfo: nil, repeats: true)
+        
+        // 初始化progressView
+        progressView = ProgressView(frame: CGRectMake(16, CGRectGetMaxY(view01.frame) + 16, CGRectGetWidth(view.frame) - 32, 3))
+        view.addSubview(progressView)
+        
+    }
+    
+    /**
+     * progressView原理
+     */
+    private func progressView01() {
         
         // 延迟3秒执行隐式动画
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(NSEC_PER_SEC * 3)), dispatch_get_main_queue(), { () -> Void in
@@ -37,6 +59,15 @@ class ViewController: UIViewController {
             self.sublayer.frame = CGRectMake(0, 0, 100, 3)
             self.sublayer.backgroundColor = UIColor.purpleColor().CGColor
         })
+    }
+    
+    /**
+     * 使用控件随机改变进度
+     */
+    func progressView02() {
+        
+        let progressValue = Float(arc4random() % 100) / 100
+        progressView.progressValue = progressValue
     }
 
     override func didReceiveMemoryWarning() {
