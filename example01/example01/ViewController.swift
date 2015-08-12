40adce9e466cf64d9a2656623ec09efcdb390652
changqing <chenchangqing198@126.com>
2015-08-12 14:48:23 +0800

    Nest嵌套动画执行

--- example01/example01/ViewController.swift
+++ example01/example01/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
     let view01OriginalRect  = CGRectMake(16, 32, 100, 100)
     let view01OriginalColor = UIColor.redColor()
     var view01OriginalCenter: CGPoint!
+    
+    var view02              : UIView!
+    let view02OriginalRect  = CGRectMake(16, CGRectGetHeight(UIScreen.mainScreen().bounds) - 148, 100, 100)
+    let view02OriginalColor = UIColor.magentaColor()
+    var view02OriginalCenter: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +29,19 @@ class ViewController: UIViewController {
         view01.backgroundColor  = view01OriginalColor
         view01OriginalCenter    = view01.center
         view.addSubview(view01)
+        
+        // 设置view02
+        view02                  = UIView(frame: view02OriginalRect)
+        view02.backgroundColor  = view02OriginalColor
+        view02OriginalCenter    = view02.center
+        view.addSubview(view02)
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - 按钮事件
     
     /**
      * 开始动画
@@ -33,7 +50,8 @@ class ViewController: UIViewController {
         
 //        typeOne()
 //        typeTwo()
-        typeThree()
+//        typeThree()
+        typeFour()
     }
     
     /**
@@ -41,13 +59,40 @@ class ViewController: UIViewController {
     */
     @IBAction func endAnimation() {
         
+        // 恢复view01
         view01.layer.removeAllAnimations()
         
         view01.center           = view01OriginalCenter
         view01.backgroundColor  = view01OriginalColor
-        self.view01.transform   = CGAffineTransformMakeScale(1, 1)
+        view01.transform        = CGAffineTransformMakeScale(1, 1)
         view01.alpha            = 1
         
+        // 恢复view02
+        view02.layer.removeAllAnimations()
+        
+        view02.center           = view02OriginalCenter
+        view02.backgroundColor  = view02OriginalColor
+        view02.transform        = CGAffineTransformMakeScale(1, 1)
+        view02.alpha            = 1
+        
+    }
+    
+    // MARK: - 动画执行方式
+    
+    private func view01Operation() {
+        
+        self.view01.center          = self.view.center
+        self.view01.backgroundColor = UIColor.magentaColor()
+        self.view01.transform       = CGAffineTransformMakeScale(2, 2)
+        self.view01.alpha           = 0.5
+    }
+    
+    private func view02Operation() {
+        
+        self.view02.center          = self.view.center
+        self.view02.backgroundColor = UIColor.redColor()
+        self.view02.transform       = CGAffineTransformMakeScale(2, 2)
+        self.view02.alpha           = 0.5
     }
     
     /**
@@ -58,10 +103,7 @@ class ViewController: UIViewController {
         
         UIView.animateWithDuration(2, animations: { () -> Void in
             
-            self.view01.center          = self.view.center
-            self.view01.backgroundColor = UIColor.magentaColor()
-            self.view01.transform       = CGAffineTransformMakeScale(2, 2)
-            self.view01.alpha           = 0.5
+            self.view01Operation()
         })
     }
     
@@ -73,13 +115,10 @@ class ViewController: UIViewController {
         
         UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveLinear | UIViewAnimationOptions.Repeat | UIViewAnimationOptions.Autoreverse, animations: { () -> Void in
             
-            self.view01.center          = self.view.center
-            self.view01.backgroundColor = UIColor.magentaColor()
-            self.view01.transform       = CGAffineTransformMakeScale(2, 2)
-            self.view01.alpha           = 0.5
+            self.view01Operation()
         }) { (finished) -> Void in
             
-            println("是否完成：\(finished)")
+            println("view01Operation是否完成：\(finished)")
         }
     }
     
@@ -94,21 +133,50 @@ class ViewController: UIViewController {
         UIView.setAnimationDuration(2)
         UIView.setAnimationRepeatAutoreverses(true)
         UIView.setAnimationRepeatCount(MAXFLOAT)
+        UIView.setAnimationDelegate(self)
+        UIView.setAnimationWillStartSelector(Selector("animationDidStart:"))
+        UIView.setAnimationDidStopSelector(Selector("animationDidStop:finished:"))
         
         // 操作视图
-        self.view01.center          = self.view.center
-        self.view01.backgroundColor = UIColor.magentaColor()
-        self.view01.transform       = CGAffineTransformMakeScale(2, 2)
-        self.view01.alpha           = 0.5
+        self.view01Operation()
         
         // 提交动画
         UIView.commitAnimations()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    /**
+     * 执行动画方式04
+     * Nest嵌套动画执行
+     */
+    private func typeFour() {
+        
+        UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveLinear | UIViewAnimationOptions.Repeat | UIViewAnimationOptions.Autoreverse, animations: { () -> Void in
+            
+            self.view01Operation()
+            
+            UIView.animateWithDuration(6, delay: 0, options: UIViewAnimationOptions.CurveLinear | UIViewAnimationOptions.Repeat | UIViewAnimationOptions.Autoreverse | UIViewAnimationOptions.OverrideInheritedDuration, animations: { () -> Void in
+                
+                self.view02Operation()
+            }) { (finished) -> Void in
+                
+                println("view02Operation是否完成：\(finished)")
+            }
+        }) { (finished) -> Void in
+            
+            println("view01Operation是否完成：\(finished)")
+        }
     }
-
-
+    
+    // MARK: - begin/commit animation delegate
+    
+    override func animationDidStart(anim: CAAnimation!) {
+    
+        println("animationDidStart")
+    }
+    
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        
+        println("animationDidStop")
+    }
 }
 
