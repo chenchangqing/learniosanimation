fedb70e382d9a5c682938a6d78cb09a27d86274a
changqing <chenchangqing198@126.com>
2015-08-14 13:21:35 +0800

    利用带有透明通道的图片创建maskView

--- example05/example05/ViewController.swift
+++ example05/example05/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var baseImageView   : UIImageView!  // 底图
+    private var maskImageView   : UIImageView!  // 遮罩图
+    private var resultImageView : UIImageView!  // 结合后的图片
 
+    // MARK: -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - SETUP
+    
+    private func setup() {
+        
+        setupImages()
+    }
+    
+    /**
+     * 设置图片
+     */
+    private func setupImages() {
+        
+        // 底图
+        baseImageView = UIImageView(frame: CGRectMake(16, 36, 60, 60))
+        baseImageView.image = UIImage(named: "base")
+        view.addSubview(baseImageView)
+        
+        // mask
+        maskImageView = UIImageView(frame: CGRectMake(CGRectGetMaxX(baseImageView.frame) + 16, 36, 60, 60))
+        maskImageView.image = UIImage(named: "mask")
+        view.addSubview(maskImageView)
+        
+        // 使用maskView的情况
+        resultImageView             = UIImageView(frame: CGRectMake(CGRectGetMaxX(maskImageView.frame) + 16, 36, 60, 60))
+        resultImageView.image       = UIImage(named: "base")
+        let mask                    = UIImageView(frame: CGRectMake(0, 0, 60, 60))
+        mask.image                  = UIImage(named: "mask")
+        resultImageView.maskView    = mask
+        view.addSubview(resultImageView)
+    }
 
 }
 
